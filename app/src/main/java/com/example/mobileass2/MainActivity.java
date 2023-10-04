c4bc62823f1a38599d9fa14808031b1f9e1963be
ChloeD-L <linduan1018@gmail.com>
2023-10-04 19:43:34 +1100

    Merged with main branch

--- app/src/main/java/com/example/mobileass2/MainActivity.java
+++ app/src/main/java/com/example/mobileass2/MainActivity.java
@@ -5,23 +5,46 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.mobileass2.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button logOut = findViewById(R.id.logout);
-        logOut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent =new Intent();
-                intent.setClass(MainActivity.this,LoginActivity.class);
-                startActivity(intent );
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {int id = item.getItemId();
+            if (id == R.id.home) {
+                replaceFragment(new HomeFragment());
+            } else if (id == R.id.map) {
+                replaceFragment(new MapsFragment());
+            } else if (id == R.id.profile) {
+                replaceFragment(new ProfileFragment());
             }
+
+            return true;
         });
+
+
+
+
+    }
+    private void replaceFragment (Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.commit();
 
     }
 
