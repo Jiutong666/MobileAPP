39e38ae72123fd3efb70fc8781235e1b093b8b30
ChloeD-L <linduan1018@gmail.com>
2023-10-04 18:47:11 +1100

    Basic map initial commit

---
+++ app/src/main/java/com/example/mobileass2/Item/MapItem.java
@@ -0,0 +1,70 @@
+package com.example.mobileass2.Item;
+
+public class MapItem {
+    public String id;
+    public String title;
+    public double latitude;
+    public double longitude;
+    public String type;
+    public String description;
+    public String imageUrl;
+    public String audioUrl;
+    public String videoUrl;
+    public int visitCount;
+    public int likeCount;
+
+    public MapItem() {
+    }
+
+    public MapItem(String id, String title, double latitude, double longitude, String type, String description, int visitCount, int likeCount) {
+        this.id = id;
+        this.title = title;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.type = type;
+        this.description = description;
+        this.visitCount = visitCount;
+        this.likeCount = likeCount;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getAudioUrl() {
+        return audioUrl;
+    }
+
+    public void setAudioUrl(String audioUrl) {
+        this.audioUrl = audioUrl;
+    }
+
+    public String getVideoUrl() {
+        return videoUrl;
+    }
+
+    public void setVideoUrl(String videoUrl) {
+        this.videoUrl = videoUrl;
+    }
+
+    public int getVisitCount() {
+        return visitCount;
+    }
+
+    public void setVisitCount(int visitCount) {
+        this.visitCount = visitCount;
+    }
+
+    public int getLikeCount() {
+        return likeCount;
+    }
+
+    public void setLikeCount(int likeCount) {
+        this.likeCount = likeCount;
+    }
+
+}
