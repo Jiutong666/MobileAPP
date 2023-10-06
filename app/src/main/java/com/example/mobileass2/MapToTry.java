372d7f03c58a7b4081c179df796b69e30d1b50ab
lingyikong <likong@student.unimelb.edu.au>
2023-10-06 22:46:25 +1100

    committtt...

---
+++ app/src/main/java/com/example/mobileass2/MapToTry.java
@@ -0,0 +1,14 @@
+package com.example.mobileass2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MapToTry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_to_try);
+    }
+}
