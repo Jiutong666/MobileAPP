12b736daec1afb3e56fa4bead8f3f8dc34bdfcee
ChloeD-L <linduan1018@gmail.com>
2023-11-01 17:23:19 +1100

    Json permission environment added

--- app/src/main/java/com/example/mobileass2/MapToTry.java
+++ app/src/main/java/com/example/mobileass2/MapToTry.java
@@ -5,9 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -16,12 +22,39 @@ import java.util.Map;
 
 public class MapToTry extends AppCompatActivity {
 
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+    String email = "linduan1018@gmail.com";
+    String password = "123456";
+
+    String TAG = "Permission Check";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_to_try);
+
+//        mAuth.signInWithEmailAndPassword(email, password)
+//                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<AuthResult> task) {
+//                        if (task.isSuccessful()) {
+//                            // 登录成功
+//                            Log.d(TAG, "signInWithEmail:success");
+//                            FirebaseUser user = mAuth.getCurrentUser();
+//                            // 更新UI，如果需要的话
+//                        } else {
+//                            // 登录失败
+//                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+//                            Toast.makeText(MapToTry.this, "Authentication failed.",
+//                                    Toast.LENGTH_SHORT).show();
+//                            // 更新UI，显示错误消息
+//                        }
+//                    }
+//                });
+
+
 
         DocumentReference docRef = FirebaseFirestore.getInstance().document("images/3rZoQQ4IaSOZZpeVWIN0");
 
