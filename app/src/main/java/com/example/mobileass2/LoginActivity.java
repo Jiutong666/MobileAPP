4fce7b3c0704199ea30d0d03ee59a5c7002b3024
Jiutong666 <2790880930@qq.com>
2023-11-09 04:35:16 +1100

    init profile

--- app/src/main/java/com/example/mobileass2/LoginActivity.java
+++ app/src/main/java/com/example/mobileass2/LoginActivity.java
@@ -27,7 +27,6 @@ public class LoginActivity extends AppCompatActivity {
     public EditText password;
     public FirebaseAuth firebaseAuth;
 
-  
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
