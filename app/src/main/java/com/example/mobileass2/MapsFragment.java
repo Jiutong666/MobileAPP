4ffbe2f04de43efe8443c2fe5446769005cb395b
lingyikong <likong@student.unimelb.edu.au>
2023-11-10 18:20:43 +1100

    sync like numbers ...

--- app/src/main/java/com/example/mobileass2/MapsFragment.java
+++ app/src/main/java/com/example/mobileass2/MapsFragment.java
@@ -285,7 +285,7 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback{
                 marker = mMap.addMarker(new MarkerOptions()
                                 .position(position)
                                 .title(item.getType())
-                                .icon(BitmapDescriptorFactory.fromResource(R.drawable.text))
+                                .icon(BitmapDescriptorFactory.fromResource(R.drawable.text4))
                         // Here you can add more customization to your marker
                 );
             } else if ("image".equals(item.getType())) {
@@ -293,7 +293,7 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback{
                 marker = mMap.addMarker(new MarkerOptions()
                                 .position(position)
                                 .title(item.getType())
-                                .icon(BitmapDescriptorFactory.fromResource(R.drawable.image))
+                                .icon(BitmapDescriptorFactory.fromResource(R.drawable.image4))
                         // Here you can add more customization to your marker
                 );
             } else if ("video".equals(item.getType())) {
